878249d151a69ad17a2bee9523c2b137cae1fa27
AbdurahmanIdris <abduidrism@gmail.com>
2024-01-15 11:11:03 +0300

    Provider and StateProvider done

--- lib/core/router.dart
+++ lib/core/router.dart
@@ -1,7 +1,20 @@
 // import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_statemgmt/presentation/pages/home_page.dart';
 import 'package:flutter_statemgmt/presentation/pages/settings_page.dart';
 import 'package:go_router/go_router.dart';
+
+// declared a global StateProvider so that it is easier to maintain the project
+//ref used to read other providers
+/*final userNameProvider = StateProvider((ref) {
+  return "SomeName";
+});*/
+
+final nameProvider = Provider<String>((ref) {
+  return 'Hello Abdurahman!';
+});
+
+final counterProvider = StateProvider((ref) => 0);
 
 class AppRouter {
   static GoRouter configureRoutes() {
--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,23 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_statemgmt/core/router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // String name = watch(providers.userNameProvider).state;
+    // final name = ref.watch(nameProvider);
+    final count = ref.watch(counterProvider);
+
+    //using snackbar
+    ref.listen(counterProvider, ((previous, next) {
+      if (next == 5) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text('The value is $next')));
+      }
+    }));
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Homepage"),
+        //for StateProvider
+        actions: [
+          IconButton(
+              onPressed: () {
+                // ref.invalidate(counterProvider);
+                ref.refresh(counterProvider);
+              },
+              icon: const Icon(Icons.refresh)),
+        ],
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () => context.goNamed(
-            "settings",
-            pathParameters: {"name": "codemagic"},
+        child: Column(children: [
+          ElevatedButton(
+            onPressed: () => context.goNamed(
+              "settings",
+              pathParameters: {"name": "codemagic"},
+            ),
+            child: const Text('Go to Settings page'),
           ),
-          child: const Text('Go to Settings page'),
-        ),
+          // Text(name),
+          //reading the created Provider with Consumenr
+          Consumer(builder: (context, ref, child) {
+            final name = ref.watch(nameProvider);
+            return Text(name);
+          }),
+          Text(
+            // '0',
+            count.toString(),
+            style: const TextStyle(fontSize: 30),
+          ),
+        ]),
+        // child: Text(name),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // ref.read(counterProvider.notifier).state++;
+          ref.read(counterProvider.notifier).update((state) => state + 1);
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
